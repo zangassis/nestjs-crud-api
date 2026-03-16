b3c0a8e13b48de009fe2c311fd30340963167c48
zangassis <assiszang@gmail.com>
2026-03-16 08:49:13 -0300

    feat: add crud dtos

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,37 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePutUserDto } from './dto/update-put-user.dto';
+import { UpdatePatchUserDto } from './dto/update-patch-user.dto';
 
 @Controller('users')
 export class UserController {
   @Post()
-  async create(@Body() body){
+  async create(@Body() body: CreateUserDto) {
     return { body };
   }
 
   @Get()
   async listAll() {
-    return {users:[]}
+    return { users: [] };
   }
 
   @Get(':id')
-  async listOne(@Param('id') id){
-    return {user:{}, id}
+  async listOne(@Param('id') id) {
+    return { user: {}, id };
+  }
+
+  @Put(':id')
+  async update(@Body() body: UpdatePutUserDto, @Param() params) {
+    return { user: {}, body };
+  }
+
+  @Patch(':id')
+  async updateParcial(@Body() body: UpdatePatchUserDto, @Param() params) {
+    return { user: {}, body };
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id) {
+    return { user: {}, id };
   }
 }
